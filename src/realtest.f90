13e83c160c73071aca2181a576a209b1cf861895
Huseyin Ozdemir <ozdemirh@gmail.com>
2019-06-25 12:59:45 +0200

    extended realtest.f90

--- src/realtest.f90
+++ src/realtest.f90
@@ -10,10 +10,21 @@
 !!          I think, the best practice is to use d0 by default 
 !!          which gives more control over the result.
 program real_test
-
 implicit none
 
-print*, sin(27.0), dsin(27.0d0), sin(27.0d0)
+real      :: r, d
+real*4    :: r4
+real*8    :: r8
+double precision :: d8
+
+
+  r  = sin(27.0)
+  d = sin(27.0d0)
+  r4 = sin(27.0)
+  r8 = sin(27.0d0)
+  d8 = dsin(27.0d0) 
+
+print*, r, d, r4, r8, d8
 
 
 
